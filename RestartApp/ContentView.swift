1e57ff332250d97230c63d5d3c78d5d086b0b733
Luiz Felipe <felnanuke@gmail.com>
2022-01-29 19:04:33 -0300

    Initial Commit

---
+++ RestartApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestartApp
+//
+//  Created by LUIZ FELIPE ALVES LIMA on 29/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by LUIZ FELIPE ALVES LIMA on 29/01/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
